4280837e52bf1cd8d1082edd5b1744616b410cb5
Howard Stickley <howardstickley12@gmail.com>
2017-07-08 13:31:48 -0400

    User login and landing page complete

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,74 @@
+# from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
-from django.views import generic
-from django.template import RequestContext
-import datetime as dt
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from datetime import datetime as dt
 
-from django.db.models import Q
 from django.contrib.auth.models import User
-from catalog.models import Order, Product, Ailment
+from catalog.models import Product, Order, Ailment
 
+# -------------------------------------
+# Page Views
+# -------------------------------------
 def index(request):
 	context_dict={}
-	return render(request, 'catalog/index.html')
+	context_dict['num_products'] = Product.objects.all().count()
+	context_dict['num_singles'] = Product.objects.filter(isBlend=False).count()
+	context_dict['num_blends'] = Product.objects.filter(isBlend=True).count()
+	context_dict['num_ailments'] = Ailment.objects.all().count()
+	return render(request, 'catalog/index.html', context_dict)
 
-# class UserListView(generic.ListView):
-#     model = User
+
+
+# -------------------------------------
+# User Authentication
+# -------------------------------------
+def register(request):
+	registered = False
+
+	if request.method == 'POST':
+		user_form = UserForm(data=request.POST)
+
+		if user_form.is_valid():
+			user = user_form.save()
+
+			user.set_password(user.password)
+			user.save()
+			registered = True
+		else:
+			print(user_form.errors)
+	else:
+		user_form = UserForm()
+
+	return render(request, 'rango/register.html',
+		{'user_form': user_form,
+		 'registered': registered})
+
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+
+		if user:
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect(reverse('index'))
+			else:
+				return HttpResponse('Your Essential Oils account is disabled.')
+		else:
+			print('Invalid login details: {0}, {1}'.format(username, password))
+			return HttpResponse('Invalid login details supplied.<br/>Details given: <br/>'+
+				'<p style="text-indent: 25px;">Username: '+username+'</p>'+
+				'<p style="text-indent: 25px;">Password: '+password+'</p>')
+	else:
+		return render(request, 'catalog/login.html', {})
+
+@login_required
+def user_logout(request):
+	logout(request)
+	return HttpResponseRedirect(reverse('index'))
--- essential_oils/urls.py
+++ essential_oils/urls.py
@@ -4,9 +4,16 @@ from django.conf.urls import patterns, url
 from django.conf.urls import include
 from django.contrib import admin
 from django.views.generic import RedirectView
+from registration.backends.simple.views import RegistrationView
+
+class MyRegistrationView(RegistrationView):
+	def get_success_url(self, user):
+		return '/catalog/'
 
 urlpatterns = [
 	url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
     url(r'^admin/', admin.site.urls),
     url(r'^catalog/', include('catalog.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
